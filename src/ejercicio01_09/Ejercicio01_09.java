830ae1674ada064fb30a2fa3a39b8baec664d9c4
user <user@192.168.1.2>
2017-09-14 02:54:19 -0500

    0109 commit

---
+++ src/ejercicio01_09/Ejercicio01_09.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ *
+ *
+ * (Área y perímetro de un rectángulo) Escriba un programa que muestre el área y el perímetro
+perímetro de un rectángulo con el ancho de 4,5 y la altura de 7,9 usando el siguiente
+fórmula:
+área = ancho * altura
+ */
+package ejercicio01_09;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio01_09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Area = ");
+	System.out.println(4.5 * 7.9);
+	System.out.println("Perimetro = ");
+        System.out.println((4.5 + 7.9) * 2);
+    }
+    
+}
